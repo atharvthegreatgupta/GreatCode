5d40d3ca1f1aae68c3f10e774de9bb114e7d2b6e
Atharv Gupta <atharvgupta592@gmail.com>
2026-06-21 02:57:45 +0530

    Connected frontend to deployed backend

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,13 +1,26 @@
-import axios from "axios"
+// import axios from "axios"
 
-const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+// const axiosClient =  axios.create({
+//     baseURL: 'http://localhost:3000',
+//     withCredentials: true,
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// });
+
+
+// export default axiosClient;
+
+
+
+import axios from "axios";
+
+const axiosClient = axios.create({
+    baseURL: "https://b-coding-platform.onrender.com",
     withCredentials: true,
     headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
     }
 });
 
-
 export default axiosClient;
-
